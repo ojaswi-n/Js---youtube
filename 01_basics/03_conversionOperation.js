4524f8838cb0f2fa5d5304edebcb591b7ff6cb4b
ojaswi-n <2k24.csai1b.2411293@gmail.com>
2026-06-06 05:26:36 +0000

    01_basics

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score = 33;
+// console.log(typeof score);
+// console.log(typeof(score));
+let score = true
+// console.log(typeof score);
+
+let valueinnumber = Number(score)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber);
+
+//"33" =>33
+//"33ave" =>NaN
+//true => 1;false =>0
+
+let loggedtrue = "eninc";
+let loggedvalue = Boolean(loggedtrue)
+console.log(loggedvalue);
+// 1=>true; 0 =>false
+//""=> false
+//"ojaswo"=>true

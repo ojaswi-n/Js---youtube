a8134afbb108204f06983b29fc90b94ab9b43d15
ojaswi-n <2k24.csai1b.2411293@gmail.com>
2026-06-06 05:04:09 +0000

    variables and dataypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 145533
+let accountemail = "ojaswi@gamil.com"
+var accountpassword = "12345"
+accountcity = "kanpur"
+// 
+
+console.log(accountId);
+accountemail = "a@gamil.com"
+accountpassword = "122132332"
+accountcity = "kaoid";
+let state
+// console.log(accountemail);
+// console.log(accountpassword);
+// console.log(accountcity);
+console.table([accountId,accountemail,accountpassword,accountcity,state])
+/* prefer not to use var 
+because of issue in block scope and funcational scope
+*/
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) // we r using node js not browser
+
+// code reablility is must
+console.log(3
+    +3
+);
+let name = "ojaswi"
+let age = 24
+//number => 2 ^ 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null = >standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined);
+
+
+
+
+

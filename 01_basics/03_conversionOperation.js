86f6921a33eb2bd67cf018d34ac01dbdb228fcac
ojaswi-n <2k24.csai1b.2411293@gmail.com>
2026-06-06 06:59:01 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,7 +14,47 @@ let valueinnumber = Number(score)
 
 let loggedtrue = "eninc";
 let loggedvalue = Boolean(loggedtrue)
-console.log(loggedvalue);
+// console.log(loggedvalue);
 // 1=>true; 0 =>false
 //""=> false
 //"ojaswo"=>true
+//****************************OPERATION**************************** 
+let value = 33
+let negvalue = -value;
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%4);
+
+let str1 = "hello"
+let str2 = " ojaswi"
+let str3 = str1 +str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1" + 2+ 2);
+// console.log(1+2 +"2");
+
+// console.log(+true);
+
+let num1 =num2 = num3 = 4;
+// console.log(num2);
+let g = 100;
+++g;
+console.log(g)
+// prefix 
+// let a = 3 ;
+// let b = ++a;
+// console.log("a:"+ a ,"b:" +b) // a => 4 and b => 4
+//postfix 
+let a = 3 ;
+let b = a++;
+// console.log("a:"+a , "b:"+b); // a => 4 and b => 3
+
+
+
+
+

e708671946a25c990b698c62c4bcdc9bd8fa5296
ojaswi-n <2k24.csai1b.2411293@gmail.com>
2026-06-09 16:52:35 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+//primitive 
+//7 :String ,Number,Boolean , null,undefined,Symbol,BigInt
+
+const score = 100//dynamically types lang 
+const scorevalue = 100.3
+const value = false
+const outsidetemp = null
+let useremail
+const id = Symbol('123');
+const anotherid = Symbol('123')
+console.log(id === anotherid)
+
+const bigNumber = 2381476348793247032779049n
+const heros = ["shaktiman","dagggaa","nagraj"] //Array
+let obj = {
+    name : "ojaswi",
+    age: 22, 
+}
+console.log(typeof bigNumber);
+console.log(typeof outsidetemp)
+console.log(typeof scorevalue)
+let myfunction = function(){
+    console.log("hihiihidhiahfhij")
+}
+myfunction()
+console.log(typeof myfunction)
+
+
+//Reference or non primitive
+//Array,Objects,Functions

30cf2c53f0d498cdc780116ef773140cbe393046
ojaswi-n <2k24.csai1b.2411293@gmail.com>
2026-06-09 16:52:05 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(3<1);
+// console.log(3===3);
+// console.log(4 >=3);
+
+// console.log("2">1);
+// console.log("01">3);
+
+
+// console.log(null >0);
+// console.log(null == 0)
+// console.log(null >= 0);// double equal to works diff from comparison in js 
+
+// double equal and triple equal works differntly
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// both undefined and null should be avoided to create confusion
+
+
+// === => strictly check krta h and datatypes are also checked 
+console.log("2" === 2);
+
+

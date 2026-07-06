54c7281b14912347a44be9e3bb21847274c9d170
ojaswi-n <2k24.csai1b.2411293@gmail.com>
2026-07-06 16:53:35 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,14 @@
+// nullish coalescing Operator (??) :null  undefined
+
+let val1 ;
+// val1 = 5 ?? 10 // o/p -> 5
+// val1 = null ?? 10 // 0/p-> 10
+// val1 = undefined ?? 20 // o/p-> 20
+val1 = undefined ?? 20 ?? 10 // o/p-> return 1st value 
+
+
+// console.log(val1); // 
+
+
+// ++++ ternary Operator
+// condition ? true : false;

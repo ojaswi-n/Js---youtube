0ecbb25016eac3bb30eadfdf47d5ff60f14ec8cc
ojaswi-n <2k24.csai1b.2411293@gmail.com>
2026-07-07 14:51:22 +0000

    switch

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -6,3 +6,38 @@
 //     default:
 //         break;
 // }
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");   // short cut -> shift + option + down arrow
+        break;
+    case "april":
+        console.log("april");   // short cut -> 
+        break;
+    case "april":
+        console.log("april");   // short cut -> 
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}

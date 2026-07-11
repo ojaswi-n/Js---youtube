af205702a915811ed8a5b58e05c4f2c3947ade52
ojaswi-n <2k24.csai1b.2411293@gmail.com>
2026-07-11 16:40:24 +0000

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,23 @@
+// array.forEach(element => {
+    
+// });
+
+let coding = ["js","python","ruby","rust"];
+// coding.forEach(function(val){ // here we r passing a callback function 
+//     console.log(val)
+// })
+// coding.forEach( (val)=> {
+//     console.log(val)
+// })// here we r passing a callback function 
+    
+// function print(val){
+//     console.log(val)
+// }
+// coding.forEach(print);
+
+
+coding.forEach((item,index,arr)=>{
+    // console.log(item,index,arr)
+})
+
+const multipleobj

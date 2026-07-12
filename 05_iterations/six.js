5a93b13317a64944bdfadbaf0fa8d23b8ee8b44f
ojaswi-n <2k24.csai1b.2411293@gmail.com>
2026-07-12 17:15:23 +0000

    six

---
+++ 05_iterations/six.js
@@ -0,0 +1,8 @@
+// const coding = ["js","ruby","java","rust","python"];
+// let value = coding.forEach( (item) =>{
+//     // console.log(item)
+//     return item // still gives undefined 
+// })
+// console.log(value) // undefined
+
+
